476e058a96479de5baa67ef210b80478f276a23e
Illya Klymov <xanf@xanf.me>
2025-02-17 07:20:23 +0200

    Requirements 17.02

---
+++ src/table.js
@@ -0,0 +1,60 @@
+function checkWin(table) {
+  const rows = table.querySelectorAll("tr");
+
+  for (let i = 0; i < rows.length; i++) {
+    let win = true;
+    const cells = rows[i].querySelectorAll("td");
+    for (let j = 0; j < cells.length; j++) {
+      if (!cells[j].classList.contains("selected")) {
+        win = false;
+        break;
+      }
+    }
+    if (win) {
+      alert("BINGO");
+      return;
+    }
+  }
+
+  const num = rows.length;
+
+  for (let i = 0; i < num; i++) {
+    let win = true;
+
+    for (let j = 0; j < num; j++) {
+      const cell = rows[j].querySelectorAll("td")[i];
+      if (!cell.classList.contains("selected")) {
+        win = false;
+        break;
+      }
+    }
+    if (win) {
+      alert("BINGO");
+      return;
+    }
+  }
+}
+
+export function generateTable(num) {
+  const table = document.createElement("table");
+  const tbody = document.createElement("tbody");
+  table.appendChild(tbody);
+
+  for (let i = 0; i < num; i++) {
+    const tr = document.createElement("tr");
+    for (let j = 0; j < num; j++) {
+      const td = document.createElement("td");
+      td.textContent = i * num + j;
+      tr.appendChild(td);
+
+      td.addEventListener("click", () => {
+        td.classList.toggle("selected");
+
+        checkWin(table);
+      });
+    }
+    tbody.appendChild(tr);
+  }
+
+  return table;
+}
